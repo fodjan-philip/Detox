47bd502848850df3559254542ce7dba4e2c39b46
Miki Leib <michaelle@wix.com>
2020-12-01 10:15:20 +0200

    Create react-native.config.js

---
+++ react-native.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dependency: {
+    platforms: {
+      ios: {
+        project: null
+      },
+      android: {
+        sourceDir: './android/detox'
+      }
+    }
+  }
+};
